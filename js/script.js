9aa81e2289193d702359c372d8fe9dd1820ba78e
screetox <80164220+screetox@users.noreply.github.com>
2025-11-10 18:41:50 +0100

    removed logo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 document.getElementById('copyright').innerHTML = '© 2020-' + new Date().getFullYear() + ' screetox.de';
+
+let emotes = ['facepalm', 'lol', 'notlikethis', 'pog', 'shock'];
+let images = document.getElementsByClassName('website-logo');
+for (var i = 0; i < images.length; i++){
+    let index = Math.floor(Math.random() * emotes.length);
+    images[i].src = images[i].src.slice(0, -7) + emotes[index] + '.png';
+    emotes.splice(index, 1);
+}
